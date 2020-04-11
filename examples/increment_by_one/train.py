59836b88d25503227158d94e2144f13dceed0fd9
Phil Wang <lucidrains@gmail.com>
2020-04-10 20:19:55 -0700

    add sanity check example for encoder / decoder

---
+++ examples/increment_by_one/train.py
@@ -0,0 +1,54 @@
+import torch
+from sinkhorn_transformer.sinkhorn_transformer import SinkhornTransformerLM
+from sinkhorn_transformer.autoregressive_wrapper import AutoregressiveWrapper
+
+N_BATCH = 32
+SRC_SEQ_LEN = 512
+TGT_SEQ_LEN = 512
+
+enc = SinkhornTransformerLM(
+    num_tokens = 64,
+    dim = 512,
+    depth = 1,
+    heads = 8,
+    max_seq_len = SRC_SEQ_LEN,
+    buckets = 16,
+    return_embeddings = True,
+    attn_sort_net = True,
+    non_permutative = True
+).cuda()
+
+dec = SinkhornTransformerLM(
+    num_tokens = 64,
+    dim = 512,
+    depth = 2,
+    heads = 8,
+    max_seq_len = TGT_SEQ_LEN,
+    buckets = 16,
+    causal = True,
+    receives_context = True,
+    attn_sort_net = True,
+    non_permutative = True
+).cuda()
+
+dec = AutoregressiveWrapper(dec, ignore_index = 0, pad_value = 0)
+opt = torch.optim.Adam([*enc.parameters(), *dec.parameters()], lr=2e-4)
+
+bos = 1 * torch.ones(N_BATCH, 1).long()
+eos = 2 * torch.ones(N_BATCH, 1).long()
+pos = 3 * torch.ones(N_BATCH, 1).long()
+
+for i in range(10000):
+    train_seq_in = torch.randint(4, 63, (N_BATCH, SRC_SEQ_LEN-2)).long()
+    train_seq_out = train_seq_in
+
+    x = torch.cat([bos, train_seq_in, eos], dim=1).cuda()
+    y = torch.cat([bos, train_seq_out, eos, eos], dim=1).cuda()
+
+    context = enc(x)
+    loss = dec(y, context = context, return_loss = True)
+    loss.backward()
+
+    opt.step()
+    opt.zero_grad()
+    print(i, loss.item())
